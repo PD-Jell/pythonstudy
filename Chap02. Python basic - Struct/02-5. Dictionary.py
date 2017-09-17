80db2a1a2f752c829310ae83a2407a3fdcf2a182
PD-Jell <mystral7@naver.com>
2017-09-17 21:27:38 +0900

    Dictionary Complete.

--- Chap02. Python basic - Struct/02-5. Dictionary.py
+++ Chap02. Python basic - Struct/02-5. Dictionary.py
@@ -35,5 +35,25 @@ print(a) # 'b' or 'a', 어떤 것이 삭제 될 지는 예측할 수 없다. 또
 # 딕셔너리 관련 함수들
 # Key 리스트 만들기 (keys)
 a = {'name': 'pey', 'phone': '0109993323', 'birth': '1118'}
-print(a.keys()) # dict_keys(['name', 'phone', 'birth'])
+print(a.keys()) # dict_keys(['name', 'phone', 'birth']), 3.0때 추가된 자료형(메모리 낭비를 방지하기 위해)
+print(list(a.keys())) # 이렇게 하면 리스트 자료형으로 불러올 수 있음.
 
+# Value 리스트 만들기 (values)
+print(a.values()) # 이건 key와 반대로 values를 뽑는 코드.
+print(list(a.values())) # 위와 같이 리스트 자료형으로 변환.
+
+# Key-Value쌍 얻기 (items)
+print(a.items()) #Key-Value 모두 반환.
+
+# Key-Value쌍 모두 지우기 (clear)
+#a.clear()
+#print(a) # 모두 지워진다.
+
+# Key로 Value 얻기 (get)
+print(a.get('name'))
+print(a.get('phone'))
+
+print(a.get('foo', 'bar')) # 값이 없을때에는 미리 정해둔 value를 반환한다.
+
+# 해당 Key가 딕셔너리 안에 있는 지 조사하기 (in)
+print('name' in a) # True. 키가 있으면 참, 없으면 거짓을 반환한다.
